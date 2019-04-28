0e5fdef1a15a7d74f755a17be3b901938a48c4f6
lautrix <laugic@outlook.com>
2019-04-28 17:21:28 +0800

    a filter

---
+++ zuul_service/src/main/java/com/lautrix/zuul_service/utils/AddResponseHeaderFilter.java
@@ -0,0 +1,33 @@
+package com.lautrix.zuul_service.utils;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+
+public class AddResponseHeaderFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletResponse servletResponse = context.getResponse();
+        servletResponse.addHeader("X-Sample", UUID.randomUUID().toString());
+        return null;
+    }
+}
